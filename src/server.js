6679c9e463137a011026be0e02c3f17dd8abbcd7
agabil4c <agabil4c@gmail.com>
2023-06-30 14:11:46 +0300

    added express

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
